22f61460bf2120d9c7b554880530593fbdaead97
vinhjaxt <ngoc.lo.dang@gmail.com>
2022-07-26 16:10:00 +0700

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -14,27 +14,19 @@ rm -rf /tmp/certbot 2>/dev/null
 mkdir -p /tmp/certbot "${DIR}/haproxy/certs" 2>/dev/null
 chmod 777 /tmp/certbot -R 2>/dev/null
 
+CERT_NAME="vncert-http"
+
 docker run --rm \
   --name="certbot-standalone" \
   --hostname="certbot-standalone" \
   --net="vncert_certbot_private" \
   -v "/tmp/certbot:/etc/letsencrypt/archive:rw" \
   certbot/certbot certonly --non-interactive --agree-tos --email dtvinh@vncert.vn --standalone \
-  -d vncert.vn -d vncert.gov.vn -d www.vncert.vn -d www.vncert.gov.vn
-  # "${hostnames[i]}"
+  --cert-name "${CERT_NAME}"
+  -d vncert.gov.vn -d webinar.vncert.vn -d www.vncert.gov.vn
 
-# hostnames=(vncert.vn vncert.gov.vn www.vncert.vn www.vncert.gov.vn)
-hostnames=(vncert.vn)
-for i in "${!hostnames[@]}"; do
-  domain="${hostnames[i]}"
-  echo 'Certbot for '"${domain}"
-  if [ ! -f "/tmp/certbot/${domain}/fullchain1.pem" ]; then
-    echo 'Generate certificate failed!'
-    exit 1
-  fi
-  cat "/tmp/certbot/${domain}/fullchain1.pem" "/tmp/certbot/${domain}/privkey1.pem" > "${DIR}/haproxy/certs/${domain}.pem"
-  rm -rf "/tmp/certbot/${domain}"
-done
+cat "/tmp/certbot/${CERT_NAME}/fullchain1.pem" "/tmp/certbot/${CERT_NAME}/privkey1.pem" > "${DIR}/haproxy/certs/${CERT_NAME}.pem"
+rm -rf "/tmp/certbot/${CERT_NAME}"
 
 docker kill haproxy
 docker start haproxy
